396e0d80be5cca91231ce14e199a8d41a106ebfb
Jun Omae <jun66j5@gmail.com>
2015-07-21 00:38:49 +0900

    python3: fixed `get_pkginfo()` and `get_sources()` not working on Python 3

--- trac/util/__init__.py
+++ trac/util/__init__.py
@@ -725,7 +725,13 @@ def get_sources(path):
                            if any(src.startswith(top) for top in toplevels))
             continue
         if dist.has_metadata('RECORD'):  # *.dist-info/RECORD
-            reader = csv.reader(StringIO(dist.get_metadata('RECORD')))
+            import csv
+            metadata = dist.get_metadata('RECORD')
+            if six.PY2:
+                metadata = io.BytesIO(metadata)
+            else:
+                metadata = io.StringIO(metadata)
+            reader = csv.reader(metadata)
             sources.update((row[0], dist)
                            for row in reader if any(row[0].startswith(top)
                                                     for top in toplevels))
@@ -758,7 +764,12 @@ def get_pkginfo(dist):
                 return any(resource_name == os.path.normpath(name)
                            for name in dist.get_metadata_lines('SOURCES.txt'))
             if dist.has_metadata('RECORD'):  # *.dist-info/RECORD
-                reader = csv.reader(io.BytesIO(dist.get_metadata('RECORD')))
+                metadata = dist.get_metadata('RECORD')
+                if six.PY2:
+                    metadata = io.BytesIO(metadata)
+                else:
+                    metadata = io.StringIO(metadata)
+                reader = csv.reader(metadata)
                 return any(resource_name == row[0] for row in reader)
             toplevel = resource_name.split('/')[0]
             if dist.has_metadata('top_level.txt'):
