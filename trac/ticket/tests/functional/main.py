ae74a689cf15d136c2792cda2f0bd01f66837817
Jun Omae <jun66j5@gmail.com>
2017-05-12 12:09:50 +0900

    (#12801) fix a test failing on Windows

--- trac/ticket/tests/functional/main.py
+++ trac/ticket/tests/functional/main.py
@@ -1777,15 +1777,15 @@ class RegressionTestTicket12801(FunctionalTwillTestCaseSetup):
             tkt = self._tester.create_ticket(
                 info={'t12801_plain': "- '''plain 1'''\n- ''plain 2''\n",
                       't12801_wiki': "- ''wiki 1''\n- '''wiki 2'''\n"})
-            tc.find("- '''plain 1'''\s*<br />- ''plain 2''")
-            tc.find('<li><em>wiki 1</em>\s*</li>'
-                    '<li><strong>wiki 2</strong>\n</li>')
+            tc.find(r"- '''plain 1'''\s*<br />- ''plain 2''")
+            tc.find(r'<li><em>wiki 1</em>\s*'
+                    r'</li><li><strong>wiki 2</strong>\s*</li>')
 
             tc.go(self._tester.url +
                   '/query?id=%s&row=t12801_plain&row=t12801_wiki' % tkt)
-            tc.find("- '''plain 1'''\s*<br />- ''plain 2''")
-            tc.find('<li><em>wiki 1</em>\s*</li>'
-                    '<li><strong>wiki 2</strong>\n</li>')
+            tc.find(r"- '''plain 1'''\s*<br />- ''plain 2''")
+            tc.find(r'<li><em>wiki 1</em>\s*'
+                    r'</li><li><strong>wiki 2</strong>\s*</li>')
 
         finally:
             for name in ('t12801_plain', 't12801_plain.format',
